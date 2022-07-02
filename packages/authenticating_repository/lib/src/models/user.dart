31cf7733c879d011087c735abf2287af846da543
Apit <apit@thezatapp.com>
2022-07-02 21:45:23 +0700

    initial commit

---
+++ packages/authenticating_repository/lib/src/models/user.dart
@@ -0,0 +1,16 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  const User({required this.id, this.email, this.name, this.photo});
+
+  final String id;
+  final String? email, name, photo;
+
+  static const empty = User(id: '');
+
+  bool get isEmpty => this == User.empty;
+
+  bool get isNotEmpty => this != User.empty;
+  @override
+  List<Object?> get props => [id, email, name, photo];
+}
